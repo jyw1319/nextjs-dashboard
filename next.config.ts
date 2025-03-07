a5d237f4e87c8e04d089cf99940118f5b488bb70
JOHN\jyw13 <jyw1319@nate.com>
2025-03-07 17:14:57 +0900

    16장까지 모두 완료

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    ppr: 'incremental'
+  },
+  devIndicators: {
+    buildActivity: false
+  } 
 };
 
 export default nextConfig;
