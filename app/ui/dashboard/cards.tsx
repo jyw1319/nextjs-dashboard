99ead16f95e8be71efd1677bf522356928a9cf96
JOHN\jyw13 <jyw1319@nate.com>
2025-03-12 15:43:47 +0900

    Revert "16장까지 모두 완료"
    This reverts commit a5d237f4e87c8e04d089cf99940118f5b488bb70.

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -1,5 +1,3 @@
-import { fetchCardData } from '@/app/lib/data';
-
 import {
   BanknotesIcon,
   ClockIcon,
@@ -16,24 +14,18 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
-  const {
-    numberOfInvoices,
-    numberOfCustomers,
-    totalPaidInvoices,
-    totalPendingInvoices,
-  } = await fetchCardData();
   return (
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
 
-      <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      />
+      /> */}
     </>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  experimental: {
-    ppr: 'incremental'
-  },
-  devIndicators: {
-    buildActivity: false
-  } 
+  /* config options here */
 };
 
 export default nextConfig;
